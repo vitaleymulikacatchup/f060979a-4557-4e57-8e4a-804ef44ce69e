b0085ea9de26e9dab94a0c8dd3f2f94c1ca4ab52
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-09-09 19:40:13 +0300

    Update project files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const interTight = Inter_Tight({
   variable: "--font-inter-tight",
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
 });
 
 const poppins = Poppins({
